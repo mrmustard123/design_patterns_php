c9b97a3c8ebe01ae6567571e4714e7f79e931102
leonardo <leonardo616@gmail.com>
2024-03-30 19:02:53 -0400

    Vamos a poner observable en todas las clases Quackables

--- implementation/compound_patterns/Observable.php
+++ implementation/compound_patterns/Observable.php
@@ -7,3 +7,44 @@
  * User: user
  */
 
+namespace compound_patterns;
+
+class Observable implements QuackObservable{
+    
+    protected array $observers;
+    
+    protected QuackObservable $duck;
+    
+    public function __construct(QuackObservable $duck) {
+        $this->duck=$duck;
+    }
+    
+    public function registerObserver($observer): void {
+        array_push($this->observers, $observer);
+    }
+    
+    public function notifyObservers(): void {
+        $iterator = new QuackableIterator();
+        
+        while($iterator->hasNext()){
+            $observer = $iterator->next();
+            //update no esta implementado todavia
+            $observer->update($this->duck);            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
